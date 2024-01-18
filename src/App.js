4a6dc849fff0f43cddbe5a1797ace81ebe92df4b
Dipanker <dipanker.pandey.03@gmail.com>
2024-01-18 14:22:49 +0530

    modified:   src/App.js 	new file:   src/SearchBox.css 	new file:   src/SearchBox.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Card from './Card.js';
 import BugDisplay from "./BugDisplay";
 import { buglist } from './buglist.js';
+import SearchBox from './SearchBox.js';
 
 class App extends React.Component {
   render() {
@@ -12,6 +13,7 @@ class App extends React.Component {
           <h1 className='heading'>Bug Tracker</h1>
           <Card />
           <h3 className='headingtwo'>Current Bugs</h3>
+          <SearchBox />
           <BugDisplay buglist={ buglist }/>
         </body>
       </div>
